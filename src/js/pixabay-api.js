6fcb994f64a31d378678910b055962a995461298
Liubov-iam <liuboviam16@gmail.com>
2024-02-23 10:36:16 +0200

    Виправляю лоадер 11 ДЗ

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,16 +1,22 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import { createGalleryMarkup } from './render-functions.js';
 
 const API_KEY = '42510436-747a3ee758966b4b69f3c3ec4';
 
+const loader = document.querySelector('.loader');
+loader.style.display = 'none';
+
 export function fetchImages(query) {
     const loader = document.querySelector('.loader');
+    const gallery = document.querySelector('.gallery');
+    
     loader.style.display = 'block';
+    gallery.innerHTML = '';  
+
     fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true`)
         .then(response => {
             if (!response.ok) {
@@ -20,7 +26,7 @@ export function fetchImages(query) {
         })
         .then(data => {
             console.log(data);
-            loader.style.display = 'none';
+           
             if (data.hits.length === 0) {
                 iziToast.info({
                     title: 'Info',
@@ -32,13 +38,13 @@ export function fetchImages(query) {
         })
         .catch(error => {
             console.error('Error fetching images:', error);
-            loader.style.display = 'none';
+            
             iziToast.error({
                 title: 'Error',
                 message: 'Failed to fetch images. Please try again later.'
             });
+        })
+        .finally(() => {
+            loader.style.display = 'none'; 
         });
 }
-
-
-
